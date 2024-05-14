02421ca16e3581e7712c70e3c1db3e159401faac
Paolo Bignardi <paolo.bigno@gmail.com>
2024-05-14 17:26:36 +0200

    drop-in replacement for the snake object
    snake object constructed as an actual object with private fields to
    prevent manipulation of the snake state.

--- snake.js
+++ snake.js
@@ -1,58 +1,82 @@
-function Snake() {
-	this.x = 0;
-	this.y = 0;
-	this.xspeed = 1;
-	this.yspeed = 0;
-	this.total = 0;
-	this.tail = [];
+"use strict";
+class Snake {
+	#x;
+	#y;
+	#xspeed;
+	#yspeed;
+	#total;
+	#tail;
 
-	this.eat = function (pos) {
+	constructor() {
+		this.#x = 0;
+		this.#y = 0;
+		this.#xspeed = 1;
+		this.#yspeed = 0;
+		this.#total = 0;
+		this.#tail = [];
+	}
+
+	// getters
+	get x() { return this.#x }
+	get y() { return this.#y }
+	get xspeed() { return this.#xspeed }
+	get yspeed() { return this.#yspeed }
+	get total() { return this.#total }
+	get tail() { return this.#tail }
+
+	// setters
+	set xspeed(xspeed) { if(abs(xspeed) <= 1) { this.#xspeed = xspeed; } }
+	set yspeed(yspeed) { if(abs(yspeed) <= 1) { this.#yspeed = yspeed; } }
+
+	// methods
+	eat(pos) {
 		let d = dist(this.x, this.y, pos.x, pos.y);
-		if (d < 1) {
-			this.total++;
-			return true;
-		} else {
-			return false;
-		}
-	};
+		if (d < 1) { this.#total++; return true; }
+		else { return false; }
+	}
 
-	this.dir = function (x, y) {
+	dir(x, y) {
 		this.xspeed = x;
 		this.yspeed = y;
-	};
+	}
 
-	this.death = function () {
+	death() {
 		for (let i = 0; i < this.tail.length; i++) {
 			let pos = this.tail[i];
 			let d = dist(this.x, this.y, pos.x, pos.y);
 			if (d < 1) {
 				console.log("starting over");
-				this.total = 0;
-				this.tail = [];
+				this.reset();
 			}
 		}
-	};
+	}
 
-	this.update = function () {
-		for (let i = 0; i < this.tail.length - 1; i++) {
-			this.tail[i] = this.tail[i + 1];
-		}
-		if (this.total >= 1) {
-			this.tail[this.total - 1] = createVector(this.x, this.y);
-		}
+	reset() {
+		this.#x = 0;
+		this.#y = 0;
+		this.#xspeed = 1;
+		this.#yspeed = 0;
+		this.#total = 0;
+		this.#tail = [];
+	}
 
-		this.x = this.x + this.xspeed * scl;
-		this.y = this.y + this.yspeed * scl;
+	update() {
+		this.#tail.unshift(createVector(this.x, this.y));
+		this.#tail = this.tail.slice(0, this.total);
 
-		this.x = constrain(this.x, 0, width - scl);
-		this.y = constrain(this.y, 0, height - scl);
-	};
+		this.#x = this.x + this.xspeed * scl;
+		this.#y = this.y + this.yspeed * scl;
 
-	this.show = function () {
+		this.#x = constrain(this.x, 0, width - scl);
+		this.#y = constrain(this.y, 0, height - scl);
+	}
+
+	show() {
 		fill(255);
 		for (let i = 0; i < this.tail.length; i++) {
 			rect(this.tail[i].x, this.tail[i].y, scl, scl);
 		}
 		rect(this.x, this.y, scl, scl);
-	};
+	}
+
 }
