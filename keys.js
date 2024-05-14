e9a09114537a9323e2c28c90d0b28d2ba7433bf5
Paolo Bignardi <paolo.bigno@gmail.com>
2024-05-15 00:17:15 +0200

    using snake own logic for change of direction
    using the logic the the changeDirection method in the Snake object to
    change direction to localise the code.

---
+++ keys.js
@@ -0,0 +1,12 @@
+let dirs = {
+	// arrows
+	37: 'LEFT',
+	39: 'RIGHT',
+	38: 'UP',
+	40: 'DOWN',
+	// wasd
+	65: 'LEFT',
+	68: 'RIGHT',
+	87: 'UP',
+	83: 'DOWN',
+}
--- sketch.js
+++ sketch.js
@@ -33,13 +33,7 @@ function draw() {
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW) {
-		s.dir(0, -1);
-	} else if (keyCode === DOWN_ARROW) {
-		s.dir(0, 1);
-	} else if (keyCode === RIGHT_ARROW) {
-		s.dir(1, 0);
-	} else if (keyCode === LEFT_ARROW) {
-		s.dir(-1, 0);
+	if (keyCode in dirs) {
+		s.changeDirection(dirs[keyCode]);
 	}
 }
