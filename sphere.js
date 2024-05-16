f4befa22d62eede618d083716a7b62fb26dcca23
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-05-17 00:12:52 +0200

    first attemp to make a sphere in 3D. I think it looks ok to play the game on the 3D embeddings of the surface with mk, keyboard to move the snake head (like in the square with the identified boundary) and the mouse to move the camera.

---
+++ sphere.js
@@ -0,0 +1,39 @@
+let width = 600;
+let height = 600;
+let scale = 20;
+function setup() {
+	createCanvas(width, height, WEBGL);
+	angleMode(RADIANS);
+
+	// Set up the camera
+	// camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+	camera(0, 550, 600, 0, 0, 0, 0, 1, 0);
+}
+
+function draw() {
+	background(30);
+	orbitControl();
+
+	increse_phi = PI / 36;
+	increse_theta = PI / 18;
+	for (let phi = 0; phi < 2 * PI; phi += increse_phi) {
+		for (let theta = 0; theta < PI; theta += increse_theta) {
+			beginShape(TESS);
+			for (i = 0; i < 2; i++) {
+				for (j = 0; j < 2; j++) {
+					let pX =
+						250 *
+						sin(theta + i * increse_theta) *
+						cos(phi + ((i + j) % 2) * increse_phi);
+					let pY =
+						250 *
+						sin(theta + i * increse_theta) *
+						sin(phi + ((i + j) % 2) * increse_phi);
+					let pZ = 250 * cos(theta + i * increse_theta);
+					vertex(pX, pY, pZ);
+				}
+			}
+			endShape();
+		}
+	}
+}
