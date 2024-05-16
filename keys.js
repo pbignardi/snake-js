e78005bab7ab579d7873ec0bcc1af41d3fd846fd
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-05-16 21:52:33 +0200

    hjkl movement

--- keys.js
+++ keys.js
@@ -1,12 +1,17 @@
 let dirs = {
 	// arrows
-	37: 'LEFT',
-	39: 'RIGHT',
-	38: 'UP',
-	40: 'DOWN',
+	37: "LEFT",
+	39: "RIGHT",
+	38: "UP",
+	40: "DOWN",
 	// wasd
-	65: 'LEFT',
-	68: 'RIGHT',
-	87: 'UP',
-	83: 'DOWN',
-}
+	65: "LEFT",
+	68: "RIGHT",
+	87: "UP",
+	83: "DOWN",
+	//hjkl
+	72: "LEFT",
+	76: "RIGHT",
+	75: "UP",
+	74: "DOWN",
+};
