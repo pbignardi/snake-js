ae526c54193754b05cc09716443cd89aa06eb0ed
Paolo Bignardi <paolo.bigno@gmail.com>
2024-05-16 11:33:36 +0200

    add topologies. cherry-pick commit a244d56
    topologies for square, cylinder, mobius, torus, klein bottle.

--- snake.js
+++ snake.js
@@ -14,6 +14,7 @@ class Snake {
 	};
 
 	constructor() {
+		console.log(topology);
 		this.#x = 0;
 		this.#y = 0;
 		this.#xspeed = 1;
@@ -75,8 +76,48 @@ class Snake {
 		this.#x = this.x + this.xspeed * scl;
 		this.#y = this.y + this.yspeed * scl;
 
-		this.#x = constrain(this.x, 0, width - scl);
-		this.#y = constrain(this.y, 0, height - scl);
+		switch (topology) {
+			case "square":
+				// Square topology
+				this.#x = constrain(this.x, 0, width - scl);
+				this.#y = constrain(this.y, 0, height - scl);
+				break;
+			case "cylinder":
+				// Cylinder topology
+				this.#x = this.x >= 0 ? this.x % width : width - scl;
+				this.#y = constrain(this.y, 0, height - scl);
+				break;
+			case "mobius":
+				// Mobius strip topology
+				let tmp_x = this.x >= 0 ? this.x % width : width - scl;
+				if (tmp_x === this.x) {
+					this.#y = constrain(this.y, 0, height - scl);
+					this.#x = tmp_x;
+				} else {
+					this.#x = tmp_x;
+					this.#y = height - scl - this.y;
+				}
+				break;
+			case "torus":
+				// Torus topology
+				this.#x = this.x >= 0 ? this.x % width : width - scl;
+				this.#y = this.y >= 0 ? this.y % height : height - scl;
+				break;
+			case "klein":
+				// Klein bottle topology
+				let tmp_y = this.y >= 0 ? this.y % height : height - scl;
+				if (tmp_y === this.y) {
+					this.#x = this.x >= 0 ? this.x % width : width - scl;
+					this.#y = tmp_y;
+				} else {
+					this.#y = tmp_y;
+					this.#x = width - scl - this.x;
+				}
+				break;
+			default:
+				console.log("Topology not found");
+				break;
+		}
 	}
 
 	show() {
