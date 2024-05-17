1c1b039b75afed0fbdf31fd59525f4d92172efe0
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-05-17 10:38:00 +0200

    add mobius strip as geometry

--- mobius.js
+++ mobius.js
@@ -2,6 +2,7 @@
 let width = 800;
 let height = 800;
 let scale = 20;
+let scale_mobius = 100;
 function setup() {
 	createCanvas(width, height, WEBGL);
 	angleMode(RADIANS);
@@ -16,4 +17,27 @@ function draw() {
 	orbitControl();
 
 	// Mobius strip see --> https://mathweb.ucsd.edu/~jeggers/math31ch/mobius_strip.pdf
+	let theta_step = (2 * PI) / 50;
+	let t_step = 2 / 30;
+	for (let theta = -PI; theta < PI; theta += theta_step) {
+		for (let t = -1; t < 1 - t_step; t += t_step) {
+			beginShape(TESS);
+			for (let i = 0; i < 2; i++) {
+				for (let j = 0; j < 2; j++) {
+					let pX =
+						cos(theta + i * theta_step) *
+						(2 +
+							(t + ((i + j) % 2) * t_step) * cos((theta + i * theta_step) / 2));
+					let pY =
+						sin(theta + i * theta_step) *
+						(2 +
+							(t + ((i + j) % 2) * t_step) * cos((theta + i * theta_step) / 2));
+					let pZ =
+						(t + ((i + j) % 2) * t_step) * sin((theta + i * theta_step) / 2);
+					vertex(scale_mobius * pX, scale_mobius * pY, scale_mobius * pZ);
+				}
+			}
+			endShape(CLOSE);
+		}
+	}
 }
