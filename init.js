f5149ee31301382c7f161377b2ea29f8e50261f5
Paolo Bignardi <paolo.bigno@gmail.com>
2024-09-20 09:52:12 +0200

    Revert "set scaling to fit sketches into view"
    This reverts commit cc84f2bcb22128c9c93bc6ff07541512830166e3.

--- init.js
+++ init.js
@@ -2,11 +2,6 @@ import { gameSketch } from "./sketches/game.js";
 import { surfaceSketch } from "./sketches/surface.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let clientHeight = document.documentElement.clientHeight;
-    let scaling = (clientHeight * 0.7) / 600;
-    document.getElementById("leftComponent").setAttribute("style", `transform: scale(${scaling});`);
-    document.getElementById("rightComponent").setAttribute("style", `transform: scale(${scaling});`);
-
 	new p5(gameSketch, document.getElementById("game"));
 	new p5(surfaceSketch, document.getElementById("surface"));
 });
