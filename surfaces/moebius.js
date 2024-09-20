0b715915983ae67a1661d703ac58c85cb03efb8d
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-09-20 12:03:08 +0200

    add identification on the surface for the cylinder, moebius, torus

--- surfaces/moebius.js
+++ surfaces/moebius.js
@@ -26,24 +26,28 @@ export function draw_moebius(
 	sketch.background(41, 41, 41);
 	sketch.orbitControl();
 
-	// Moebius strip see --> https://mathweb.ucsd.edu/~jeggers/math31ch/moebius_strip.pdf
+	// Moebius strip
 	let theta_step = (2 * sketch.PI) / cols;
 	let t_step = 2 / rows;
-    let head_color = sketch.color(195, 232, 141) // to change ?
-    let last_color = sketch.color(49, 128, 25) // to change ?
+	let startColor_1 = sketch.color(255, 203, 107);
+	let endColor_1 = sketch.color(255, 83, 112);
+	let head_color = sketch.color(195, 232, 141); // to change ?
+	let last_color = sketch.color(49, 128, 25); // to change ?
 
 	for (let theta_num = 0; theta_num < cols; theta_num += 1) {
 		for (let t_num = 0; t_num < rows; t_num += 1) {
 			let theta = (theta_num - cols / 2) * theta_step;
 			let t = (t_num - rows / 2) * t_step;
 
-			sketch.beginShape(sketch.TESS);
 			// Color of the face
-            let index = snakePosition !== null ? snakePosition.findIndex((e) => (e[0] == theta_num && e[1] == t_num)) : -1;
-            if (index > -1) {
-                let inter = sketch.map(index, 0, snakePosition.length, 0, 1);
-                let color = sketch.lerpColor(head_color, last_color, inter);
-                sketch.fill(color);
+			let index =
+				snakePosition !== null
+					? snakePosition.findIndex((e) => e[0] == theta_num && e[1] == t_num)
+					: -1;
+			if (index > -1) {
+				let inter = sketch.map(index, 0, snakePosition.length, 0, 1);
+				let color = sketch.lerpColor(head_color, last_color, inter);
+				sketch.fill(color);
 			} else if (equalArray(foodPosition, [theta_num, t_num]) || false) {
 				sketch.fill(240, 113, 120);
 			} else if ((theta_num + t_num) % 2 == 0) {
@@ -53,6 +57,7 @@ export function draw_moebius(
 			}
 
 			// Construction of the face with the four vertices
+			sketch.beginShape(sketch.TESS);
 			for (let i = 0; i < 2; i++) {
 				for (let j = 0; j < 2; j++) {
 					let pX =
@@ -78,6 +83,36 @@ export function draw_moebius(
 				}
 			}
 			sketch.endShape(sketch.CLOSE);
+
+			// Construction of the identification
+			if (theta === -sketch.PI) {
+				let pX = x_shift + sketch.cos(theta) * (2 + t * sketch.cos(theta / 2));
+				let pY = y_shift + sketch.sin(theta) * (2 + t * sketch.cos(theta / 2));
+				let pZ = t * sketch.sin(theta / 2);
+
+				let pX2 =
+					x_shift +
+					sketch.cos(theta) * (2 + (t + t_step) * sketch.cos(theta / 2));
+				let pY2 =
+					y_shift +
+					sketch.sin(theta) * (2 + (t + t_step) * sketch.cos(theta / 2));
+				let pZ2 = (t + t_step) * sketch.sin(theta / 2);
+
+				let inter = sketch.map(t_num, 0, rows, 0, 1);
+				let color = sketch.lerpColor(startColor_1, endColor_1, inter);
+				sketch.stroke(color);
+				sketch.strokeWeight(5);
+				sketch.line(
+					scale_moebius * pX,
+					scale_moebius * pY,
+					scale_moebius * pZ,
+					scale_moebius * pX2,
+					scale_moebius * pY2,
+					scale_moebius * pZ2
+				);
+				sketch.stroke("black");
+				sketch.strokeWeight(1);
+			}
 		}
 	}
 }
--- surfaces/torus.js
+++ surfaces/torus.js
@@ -11,7 +11,7 @@ export function setup_torus(sketch, width_surface, height_surface) {
 
 	// Set up the camera
 	// camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
-	sketch.camera(0, 550, -450, 0, 0, 0, 0, 1, 0);
+	sketch.camera(0, 600, -500, 0, 0, 0, 0, 1, 0);
 }
 
 export function draw_torus(
@@ -23,10 +23,14 @@ export function draw_torus(
 	colorChessboard1,
 	colorChessboard2
 ) {
+	let startColor_1 = sketch.color(255, 203, 107);
+	let endColor_1 = sketch.color(255, 83, 112);
+	let startColor_2 = sketch.color(195, 232, 141);
+	let endColor_2 = sketch.color(130, 170, 255);
 	sketch.background(41, 41, 41);
 	sketch.orbitControl();
-    let head_color = sketch.color(195, 232, 141) // to change ?
-    let last_color = sketch.color(49, 128, 25) // to change ?
+	let head_color = sketch.color(195, 232, 141); // to change ?
+	let last_color = sketch.color(49, 128, 25); // to change ?
 
 	let increse_theta = (2 * sketch.PI) / cols;
 	let increse_phi = (2 * sketch.PI) / rows;
@@ -36,13 +40,15 @@ export function draw_torus(
 			let theta = theta_num * increse_theta;
 			let phi = phi_num * increse_phi;
 
-			sketch.beginShape(sketch.TESS);
 			// Color of the face
-            let index = snakePosition !== null ? snakePosition.findIndex((e) => (e[0] == theta_num && e[1] == phi_num)) : -1;
-            if (index > -1) {
-                let inter = sketch.map(index, 0, snakePosition.length, 0, 1);
-                let color = sketch.lerpColor(head_color, last_color, inter);
-                sketch.fill(color);
+			let index =
+				snakePosition !== null
+					? snakePosition.findIndex((e) => e[0] == theta_num && e[1] == phi_num)
+					: -1;
+			if (index > -1) {
+				let inter = sketch.map(index, 0, snakePosition.length, 0, 1);
+				let color = sketch.lerpColor(head_color, last_color, inter);
+				sketch.fill(color);
 			} else if (equalArray(foodPosition, [theta_num, phi_num]) || false) {
 				sketch.fill(240, 113, 120);
 			} else if ((theta_num + phi_num) % 2 == 0) {
@@ -51,6 +57,7 @@ export function draw_torus(
 				sketch.fill(colorChessboard2);
 			}
 
+			sketch.beginShape(sketch.TESS);
 			// Construction of the face with the four vertices
 			for (let i = 0; i < 2; i++) {
 				for (let j = 0; j < 2; j++) {
@@ -68,6 +75,59 @@ export function draw_torus(
 				}
 			}
 			sketch.endShape();
+
+			// Construction of the identification
+			if (theta === 0) {
+				let pX =
+					(radius_hole + radius_tube * sketch.cos(phi)) * sketch.cos(theta);
+				let pY =
+					y_shift +
+					(radius_hole + radius_tube * sketch.cos(phi)) * sketch.sin(theta);
+				let pZ = radius_tube * sketch.sin(phi);
+
+				let pX2 =
+					(radius_hole + radius_tube * sketch.cos(phi + increse_phi)) *
+					sketch.cos(theta);
+				let pY2 =
+					y_shift +
+					(radius_hole + radius_tube * sketch.cos(phi + increse_phi)) *
+						sketch.sin(theta);
+				let pZ2 = radius_tube * sketch.sin(phi + increse_phi);
+
+				let inter = sketch.map(phi_num, 0, rows, 0, 1);
+				let color = sketch.lerpColor(startColor_1, endColor_1, inter);
+				sketch.stroke(color);
+				sketch.strokeWeight(5);
+				sketch.line(pX, pY, pZ, pX2, pY2, pZ2);
+				sketch.stroke("black");
+				sketch.strokeWeight(1);
+			}
+
+			if (phi === 0) {
+				let pX =
+					(radius_hole + radius_tube * sketch.cos(phi)) * sketch.cos(theta);
+				let pY =
+					y_shift +
+					(radius_hole + radius_tube * sketch.cos(phi)) * sketch.sin(theta);
+				let pZ = radius_tube * sketch.sin(phi);
+
+				let pX2 =
+					(radius_hole + radius_tube * sketch.cos(phi)) *
+					sketch.cos(theta + increse_theta);
+				let pY2 =
+					y_shift +
+					(radius_hole + radius_tube * sketch.cos(phi)) *
+						sketch.sin(theta + increse_theta);
+				let pZ2 = radius_tube * sketch.sin(phi);
+
+				let inter = sketch.map(theta_num, 0, cols, 0, 1);
+				let color = sketch.lerpColor(startColor_2, endColor_2, inter);
+				sketch.stroke(color);
+				sketch.strokeWeight(5);
+				sketch.line(pX, pY, pZ, pX2, pY2, pZ2);
+				sketch.stroke("black");
+				sketch.strokeWeight(1);
+			}
 		}
 	}
 }
