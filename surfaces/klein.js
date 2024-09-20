2fc4a256ca682d001fb8e871a23c92f0d34f849b
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-09-20 14:47:37 +0200

    add commented code for add the identification in klein bottle

--- surfaces/klein.js
+++ surfaces/klein.js
@@ -22,6 +22,10 @@ export function draw_klein(
 	colorChessboard1,
 	colorChessboard2
 ) {
+	let startColor_1 = sketch.color(255, 203, 107);
+	let endColor_1 = sketch.color(255, 83, 112);
+	let startColor_2 = sketch.color(195, 232, 141);
+	let endColor_2 = sketch.color(130, 170, 255);
 	let head_color = sketch.color(195, 232, 141); // to change ?
 	let last_color = sketch.color(49, 128, 25); // to change ?
 	sketch.background(41, 41, 41);
@@ -35,7 +39,6 @@ export function draw_klein(
 			let u = u_num * u_step;
 			let v = v_num * v_step;
 
-			sketch.beginShape(sketch.TESS);
 			// Color of the face
 			let index =
 				snakePosition !== null
@@ -54,6 +57,7 @@ export function draw_klein(
 			}
 
 			// Construction of the face with the four vertices
+			sketch.beginShape(sketch.TESS);
 			for (let i = 0; i < 2; i++) {
 				for (let j = 0; j < 2; j++) {
 					// see https://mathcurve.com/surfaces.gb/klein/klein.shtml
@@ -109,6 +113,83 @@ export function draw_klein(
 				}
 			}
 			sketch.endShape(sketch.CLOSE);
+
+			// Construction of the identification
+			// if (v === 0) {
+			// 	let r = 2 * (1 - sketch.cos(u) / 2);
+			// 	if (u < sketch.PI) {
+			// 		var pX =
+			// 			x_shift +
+			// 			scale_klein *
+			// 				(3 * (1 + sketch.sin(u)) + r * sketch.cos(v)) *
+			// 				sketch.cos(u);
+			// 		var pY = scale_klein * (4 + r * sketch.cos(v)) * sketch.sin(u);
+			// 		var pZ = scale_klein * r * sketch.sin(v);
+			// 	} else {
+			// 		var pX =
+			// 			x_shift +
+			// 			scale_klein *
+			// 				(3 * (1 + sketch.sin(u)) * sketch.cos(u) - r * sketch.cos(v));
+			// 		var pY = scale_klein * 4 * sketch.sin(u);
+			// 		var pZ = scale_klein * r * sketch.sin(v);
+			// 	}
+
+			// 	r = 2 * (1 - sketch.cos(u + u_step) / 2);
+			// 	if (u < sketch.PI) {
+			// 		var pX2 =
+			// 			x_shift +
+			// 			scale_klein *
+			// 				(3 * (1 + sketch.sin(u + u_step)) + r * sketch.cos(v)) *
+			// 				sketch.cos(u + u_step);
+			// 		var pY2 =
+			// 			scale_klein * (4 + r * sketch.cos(v)) * sketch.sin(u + u_step);
+			// 		var pZ2 = scale_klein * r * sketch.sin(v);
+			// 	} else {
+			// 		var pX2 =
+			// 			x_shift +
+			// 			scale_klein *
+			// 				(3 * (1 + sketch.sin(u + u_step)) * sketch.cos(u + u_step) -
+			// 					r * sketch.cos(v));
+			// 		var pY2 = scale_klein * 4 * sketch.sin(u + u_step);
+			// 		var pZ2 = scale_klein * r * sketch.sin(v);
+			// 	}
+
+			// 	let inter = sketch.map(u_num, 0, rows, 0, 1);
+			// 	let color = sketch.lerpColor(startColor_1, endColor_1, inter);
+			// 	sketch.stroke(color);
+			// 	sketch.strokeWeight(5);
+			// 	sketch.line(pX, pY, pZ, pX2, pY2, pZ2);
+			// 	sketch.stroke("black");
+			// 	sketch.strokeWeight(1);
+			// }
+
+			// if (u === 0) {
+			// 	let r = 2 * (1 - sketch.cos(u) / 2);
+			// 	let pX =
+			// 		x_shift +
+			// 		scale_klein *
+			// 			(3 * (1 + sketch.sin(u)) + r * sketch.cos(v)) *
+			// 			sketch.cos(u);
+			// 	let pY = scale_klein * (4 + r * sketch.cos(v)) * sketch.sin(u);
+			// 	let pZ = scale_klein * r * sketch.sin(v);
+
+			// 	let pX2 =
+			// 		x_shift +
+			// 		scale_klein *
+			// 			(3 * (1 + sketch.sin(u)) + r * sketch.cos(v + v_step)) *
+			// 			sketch.cos(u);
+			// 	let pY2 =
+			// 		scale_klein * (4 + r * sketch.cos(v + v_step)) * sketch.sin(u);
+			// 	let pZ2 = scale_klein * r * sketch.sin(v + v_step);
+
+			// 	let inter = sketch.map(v_num, 0, cols, 0, 1);
+			// 	let color = sketch.lerpColor(startColor_2, endColor_2, inter);
+			// 	sketch.stroke(color);
+			// 	sketch.strokeWeight(5);
+			// 	sketch.line(pX, pY, pZ, pX2, pY2, pZ2);
+			// 	sketch.stroke("black");
+			// 	sketch.strokeWeight(1);
+			// }
 		}
 	}
 }
